3819c53c2fb13316946534eb770c4cf5df6c892b
matesszabo <matesszabo@hotmail.com>
2024-11-15 12:02:01 +0100

    Person Repo bugfix

--- src/main/java/hu/unideb/inf/lev/SpringJavaFX/model/PersonRepository.java
+++ src/main/java/hu/unideb/inf/lev/SpringJavaFX/model/PersonRepository.java
@@ -2,5 +2,5 @@ package hu.unideb.inf.lev.SpringJavaFX.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PersonRepository extends JpaRepository<Long, Person> {
+public interface PersonRepository extends JpaRepository<Person, Long> {
 }
